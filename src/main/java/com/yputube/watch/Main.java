6e91c18f66be13444508e4079131561b3054103b
illia_dev <illya.posv.b@gmail.com>
2020-02-04 15:34:45 +0200

    javafx project

---
+++ src/main/java/com/yputube/watch/Main.java
@@ -0,0 +1,22 @@
+package main.java.com.yputube.watch;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Label work = new Label("WORK!!");
+        Pane pane = new Pane(work);
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
